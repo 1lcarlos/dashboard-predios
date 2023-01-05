0bd84fa7c5e64dadc2ecef8cab938720ba6c1073
lcarlos <luiscardozoicg@gmail.com>
2023-01-04 22:55:02 -0500

    Create every one of variables for scrap data

--- metroCuadrado/index.js
+++ metroCuadrado/index.js
@@ -1,20 +1,30 @@
-import fetch from 'node-fetch';
-import * as cheerio from 'cheerio';
+import fetch from "node-fetch";
+import * as cheerio from "cheerio";
 
-const res = await fetch('https://kingsleague.pro/estadisticas/clasificacion/');
+const res = await fetch("https://kingsleague.pro/estadisticas/clasificacion/");
 const html = await res.text();
-/* console.log(html); */
 
 const $ = cheerio.load(html);
-$('table tbody tr').each((index,el) => {
-    const rawTitulo = $(el).find('.fs-table-text_3').text();
+$("table tbody tr  ").each((index, el) => {
+  const rawTeam = $(el).find(".fs-table-text_3").text();
+  const rawWins = $(el).find(".fs-table-text_4").text();
+  const rawTLoses = $(el).find(".fs-table-text_5").text();
+  const rawGoalsScored = $(el).find(".fs-table-text_6").text();
+  const rawGoalsConceded = $(el).find(".fs-table-text_7").text();
+  const rawCardsYellow = $(el).find(".fs-table-text_8").text();
+  const rawCardsRed = $(el).find(".fs-table-text_9").text();
 
+  console.log({ rawTeam, rawWins });
+});
 
-    console.log({rawTitulo});
-})
-
-const predio = [{
-    titulo: 'nombre',
-    precio: 0,
-    area: 0
-}]
+const predio = [
+  {
+    team: "Team 1",
+    wins: 0,
+    loses: 0,
+    goalsScored: 0,
+    goalsConceded: 0,
+    cardsYellow: 0,
+    cardsRed: 0,
+  },
+];
